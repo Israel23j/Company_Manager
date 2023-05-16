b34c884eede63815bd672ae9df846552f5f6f5f9
Israel23j <119113917+Israel23j@users.noreply.github.com>
2023-05-16 23:28:07 +0200

    Add files via upload

---
+++ backend/connection/db_connect.py
@@ -0,0 +1,61 @@
+import psycopg2
+
+class Connection_database():
+
+    conn = None
+
+    def __init__(self):
+
+        try:
+            
+            self.conn = psycopg2.connect(host="database", database="test", user="postgres", password="password")
+        
+        except:
+            
+            print("Error de conexión")
+            self.conn.close()
+
+    def read_all(self,tb_name):
+
+        cur = self.conn.cursor()
+        cur.execute("""
+                    SELECT * FROM %s
+                    """ % (tb_name))
+        result = cur.fetchall()
+        
+        return result
+    
+    def read_one(self, tb_name, column_name, id):
+        
+        cur = self.conn.cursor()
+        cur.execute("""
+                    SELECT * FROM %s WHERE %s = %d
+                    """ % (tb_name,column_name,id))  
+        
+        return cur.fetchone()
+        
+    def write_customers(self, tb_name, data):
+        
+        cur = self.conn.cursor()
+        cur.execute("""
+                        INSERT INTO %s VALUES (%s)
+                    """ % (tb_name,data))
+        self.conn.commit()
+        
+    def write_products(self, data):
+        
+        cur = self.conn.cursor()
+        cur.execute("""
+                    INSERT INTO products VALUES %s
+                    """ % (data))
+        self.conn.commit()
+        
+        
+                 
+
+    def __def__(self):
+
+        self.conn.close()
+
+
+            
--- backend/main.py
+++ backend/main.py
@@ -1,40 +1,210 @@
 from fastapi import FastAPI
-import psycopg2
+from connection.db_connect import *
 
 app = FastAPI()
+app.title = "Company Manager API"
+conn = Connection_database()
 
-def conn_database():
 
-    try:
-
-        conn = psycopg2.connect(host="database", database="test", user="postgres", password="password")
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM datos;")
-        result = cursor.fetchone()
-
-    except:
-        
-        result = "Error de conexion"
-
-    return result
 
 @app.get('/')
 def message():
-
-    return {"message":"Hola Isra"}
     
-@app.get('/products')
+    data = conn.read_all("providers")
+    
+    items = {}
+
+    for row in data:
+
+        providers = {}
+        providers['name'] = row[1]
+        providers['cif'] = row[2]
+        providers['direction'] = row[3]
+        providers['phone'] = row[4]
+        providers['email'] = row[5]
+        providers['contact'] = row[6]
+        providers['schedule'] = row[7]
+        items[row[0]] = providers
+    
+    return items
+
+    
+@app.get('/products', tags=["Products"])
 def message_2():
 
-    return conn_database()
+    data = conn.read_all("products")
 
-@app.get('/products/{product_id}')
-def message_2(product_id: int):
-    return f"Product: {product_id}"
+    items = {}
+
+    for row in data:
+
+        products = {}
+        products['Code_product'] = row[0]
+        products['ID_provider'] = row[1]
+        products['name'] = row[2]
+        products['price_without_iva'] = row[3]
+        items[row[0]] = products
+        
+    return items
+
+
+@app.get('/products/{code_product}', tags=["Products"])
+def message_2(code_product: int):
     
 
-
-@app.post('/products')
-def look_product(product_id: int):
-    return f"Producto: {product_id}"
+    data = conn.read_one("products","code_product",code_product)
     
+    product = {}
+    item = {}
+    
+    item['Provider'] = data[1]
+    item['Name'] = data[2]
+    item['Price'] = data[3] 
+    product[code_product] = item
+
+    return product
+
+@app.get('/providers', tags=["Providers"])
+def get_providers():
+    
+    data = conn.read_all("providers")
+
+    items = {}
+
+    for row in data:
+
+        providers = {}
+        providers['Code_product'] = row[0]
+        providers['ID_provider'] = row[1]
+        providers['name'] = row[2]
+        providers['price_without_iva'] = row[3]
+        items[row[0]] = providers
+        
+    return items
+
+@app.get('/providers/{id_provider}', tags=["Providers"])
+def get_only_provider(id_provider:int):
+    
+    data = conn.read_one("providers","id_provider",id_provider)
+    item = {}
+    provider = {}
+    provider["name"] = data[1]
+    provider["cif"] = data[2]
+    provider["direction"] = data[3]
+    provider["phone"] = data[4]
+    provider["email"] = data[5]
+    provider["contact"] = data[6]
+    provider["schedule"] = data[7]
+    item[id_provider] = provider
+
+    return item
+
+@app.get('/clients', tags=["clients"])
+def get_clients():
+    
+    data = conn.read_all("clients")
+
+    items = {}
+
+    for row in data:
+
+        clients = {}
+        clients["name"] = row[1]
+        clients["cif"] = row[2]
+        clients["direction"] = row[3]
+        clients["phone"] = row[4]
+        clients["email"] = row[5]
+        clients["contact"] = row[6]
+        clients["schedule"] = row[7]
+        items[row[0]] = clients
+        
+    return items
+
+@app.get('/clients/{id_client}', tags=["clients"])
+def get_only_client(id_client:int):
+    
+    data = conn.read_one("clients","id_client",id_client)
+    item = {}
+    client = {}
+    client["name"] = data[1]
+    client["cif"] = data[2]
+    client["direction"] = data[3]
+    client["phone"] = data[4]
+    client["email"] = data[5]
+    client["contact"] = data[6]
+    client["schedule"] = data[7]
+    item[id_client] = client
+
+    return item
+
+@app.get('/expenses', tags=["Expenses"])
+def get_all_expense():
+    
+    data = conn.read_all("expenses")
+
+    items = []
+
+    for row in data:
+
+        expenses = {}
+        expenses['Code_product'] = row[0]
+        expenses['ID_provider'] = row[1]
+        expenses['name'] = row[2]
+        expenses['price_without_iva'] = row[3]
+        items.append(expenses)
+        
+    return items
+
+@app.get('/expenses/{id_order}', tags=["Expenses"])
+def get_only_expense(id_order:int):
+    
+    data = conn.read_one("expenses","id_order",id_order)
+    item = {}
+    expense = {}
+    expense["ID_client"] = data[1]
+    expense["date"] = data[2]
+    item[data[0]] = expense
+
+    return item
+
+@app.get('/income', tags=["Income"])
+def list_expense():
+    
+    data = conn.read_all("income")
+    
+    items = {}
+
+    for row in data:
+
+        income = {}
+        income['ID_client'] = row[1]
+        income['quantity'] = row[2]
+        income['date'] = row[3]
+        income['price_without_iva'] = row[4]
+        items[row[0]] = income
+        
+    return items
+
+@app.get('/income/{id_order}', tags=["Income"])
+def list_id_expense(id_order:int):
+
+    data = conn.read_one("income","id_order",id_order)
+    item = {}
+    order = {}
+    order["ID_client"] = data[1]
+    order["date"] = data[2]
+    item[data[0]] = order
+
+    return item
+
+
+########## INSERT ##########
+
+@app.post('/products/insert', tags=["Products"])
+def look_product():
+    conn.write_customers("clients(name,cif,direction,phone,email,contact,schedule)","'romero company','96745673A','d',132476890,'a@a','pepe','09:00-18:00'")
+    
+
+@app.post('/products/insert')
+def insert_product():
+    conn.write_products()
